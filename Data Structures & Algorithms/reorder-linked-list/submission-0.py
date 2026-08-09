b989330fbfb2a202eb8490165e24d9d2a87f624a
Võ Tú Trình <95646775+VoTuTrinh@users.noreply.github.com>
2026-08-10 00:41:04 +0700

    Update: reorder-linked-list - submission-0

--- Data Structures & Algorithms/reorder-linked-list/submission-0.py
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.py
@@ -5,12 +5,43 @@
 #         self.next = next
 
 class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
+    def reorderList(self, head: Optional[ListNode]) -> None:
         slow, fast = head, head
-
-        while fast and fast.next:
+        
+        while fast and fast.next: 
             slow = slow.next
             fast = fast.next.next
-            if slow == fast:
-                return True
-        return False
+        
+        l1 = head
+        l2 = slow.next
+        slow.next = None
+
+        l2 = self.reverse(l2)
+
+        while l2:
+            next_node1, next_node2 = l1.next, l2.next
+            l1.next = l2
+            l2.next = next_node1
+            l1, l2 = next_node1, next_node2
+    
+    def reverse(self, linked_list: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, linked_list
+
+        while curr: 
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+        
+        return prev
+
+            
+
+            
+                
+            
+
+
+        
+
+        
